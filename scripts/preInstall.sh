ede30d552c2dbe28b53ab0cbadb03927c9111c3f
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-05-16 14:37:53 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,2 +1,10 @@
 #set env vars
 #set -o allexport; source .env; set +o allexport;
+
+mkdir -p ./plugins
+mkdir -p ./upload
+mkdir -p ./limesurvey_data
+
+chown -R 1001:1001 ./plugins
+chown -R 1001:1001 ./upload
+chown -R 1001:1001 ./limesurvey_data
